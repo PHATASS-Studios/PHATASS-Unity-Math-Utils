9f6274ed05e5988619311e63e2289cf90f5e079d
HectorColasValtuena <MephistoII666@gmail.com>
2022-02-24 15:13:38 +0100

    created ScreenUtils

---
+++ ScreenUtils/ScreenUtils.cs
@@ -0,0 +1,19 @@
+using Screen = UnityEngine.Screen;
+using Vector2 = UnityEngine.Vector2;
+
+namespace CatAttack.Utils
+{
+	public static class ScreenUtils
+	{
+		//offers an easy to consume Vector2 with the pixel resolution of the screen
+		public static Vector2 screenResolution
+		{ get { return new Vector2(x: Screen.width, y: Screen.height); }}
+
+		//converts from an onscreen pixel position into a normalized (0f - 1f) position
+		//considers Screen.width & Screen.height
+		public static Vector2 PixelToNormalizedScreenPosition (Vector2 pixelPosition)
+		{
+			return pixelPosition / screenResolution;
+		}
+	}
+}
