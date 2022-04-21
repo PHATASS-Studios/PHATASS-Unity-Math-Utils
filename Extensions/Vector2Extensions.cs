1c62c41694a261641aa4d239175642e9043e20d6
HectorColasValtuena <MephistoII666@gmail.com>
2022-04-21 19:39:39 +0200

    Vector2Extensions added EFromToVector2 and EFromToVector2Normalized

--- Extensions/Vector2Extensions.cs
+++ Extensions/Vector2Extensions.cs
@@ -23,5 +23,31 @@ namespace PHATASS.Utils.Extensions
 		public static float EDistanceTo2D (this Vector3 originVector, Vector2 destinationVector)
 		{ return ((Vector2)originVector).EDistanceTo2D((Vector2) destinationVector); }
 	//ENDOF EDistanceTo2D
+
+	//EFromToVector2
+		public static Vector2 EFromToVector2 (this Vector2 fromVector, Vector2 toVector)
+		{
+			return toVector - fromVector;
+		}
+
+		// Vector3 alternatives
+		public static Vector2 EFromToVector2 (this Vector3 fromVector, Vector2 toVector)
+		{
+			return ((Vector2) fromVector).EFromToVector2(toVector);
+		}
+	//ENDOF EFromToVector2
+
+	//EFromToVector2Normalized
+		public static Vector2 EFromToVector2Normalized (this Vector2 fromVector, Vector2 toVector)
+		{
+			return fromVector.EFromToVector2(toVector).normalized;
+		}
+
+		// Vector3 alternatives
+		public static Vector2 EFromToVector2Normalized (this Vector3 fromVector, Vector2 toVector)
+		{
+			return ((Vector2) fromVector).EFromToVector2Normalized(toVector);
+		}
+	//ENDOF EFromToVector2Normalized
 	}
 }
