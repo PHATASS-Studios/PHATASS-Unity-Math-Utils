35baf30b883025fd422f5006d7873387bb37d835
HectorColasValtuena <MephistoII666@gmail.com>
2022-06-02 01:39:22 +0200

    IList Extension methods in CollectionExtensions moved to IListExtensions

--- Extensions/IListExtensions.cs
+++ Extensions/IListExtensions.cs
@@ -7,21 +7,55 @@ namespace PHATASS.Utils.Extensions
 {
 	public static class IListExtensions
 	{
-	//ERandomElement
-	// Returns a random item within this list
-		//IList<T> version
+	//Search methods
+		// Search backwards from the end of an IList<T> and return first non-null object
+		public static T EMGetLastNonNull<T> (this IList<T> list)
+		{
+			for (int i = list.Count-1; i >= 0; i--)
+			{ if (list[i] != null) return list[i]; }
+			return default(T);
+		}
+	//ENDOF Search methods
+
+
+	//Casting methods
+		// Casts all the elements of IList<TIn> into a new Array<TOut>
+		//	Any element that doesn't properly cast as TOut is replaced by a null entry
+		public static TOut[] EMListCastToArray <TIn, TOut> (this IList<TIn> list)
+			where TOut : class
+		{
+			TOut[] array = new TOut[list.Count];
+			for (int i = 0, iLimit = list.Count; i < iLimit; i++)
+			{
+				array[i] = (list[i] as TOut);
+			}
+			return array;
+		}
+
+		// Casts all the elements of IList<TIn> into a new List<TOut>
+		//	Any element that doesn't properly cast as TOut is replaced by a null entry
+		public static List<TOut> EMListCastToList <TIn, TOut> (this IList<TIn> list)
+			where TOut : class
+		{
+			return new List<TOut>(list.EMListCastToArray<TIn, TOut>());
+		}
+	//ENDOF Casting methods
+
+	//Randomization methods
+		// Returns a random item within this list
+		//  IList<T> generic version
 		public static T ERandomElement <T> (this IList<T> list)
 		{
 			if (list.Count == 0) { return default(T); }
 			return list[Random.Range(0, list.Count)];
 		}
 
-		//IList version
+		//  IList version
 		public static object ERandomElement (this IList list)
 		{
 			if (list.Count == 0) { return null; }
 			return list[Random.Range(0, list.Count)];
 		}
-	//ENDOF ERandomElement
+	//ENDOF Randomization methods
 	}
 }
