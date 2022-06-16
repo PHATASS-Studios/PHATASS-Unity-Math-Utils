d2cf2cafde5426673896ae1d8b1173769cb99346
HectorColasValtuena <MephistoII666@gmail.com>
2022-06-17 01:50:25 +0200

    ScreenUtils class renamed ScreenSize, created ScreenUtils namespace to contain it

--- ScreenUtils/ScreenSize.cs
+++ ScreenUtils/ScreenSize.cs
@@ -1,19 +1,19 @@
 using Screen = UnityEngine.Screen;
 using Vector2 = UnityEngine.Vector2;
 
-namespace PHATASS.Utils
+namespace PHATASS.Utils.ScreenUtils
 {
-	public static class ScreenUtils
+	public static class ScreenSize
 	{
 		//offers an easy to consume Vector2 with the pixel resolution of the screen
-		public static Vector2 screenResolution
+		public static Vector2 pixelResolution
 		{ get { return new Vector2(x: Screen.width, y: Screen.height); }}
 
 		//converts from an onscreen pixel position into a normalized (0f - 1f) position
 		//considers Screen.width & Screen.height
 		public static Vector2 PixelToNormalizedScreenPosition (Vector2 pixelPosition)
 		{
-			return pixelPosition / screenResolution;
+			return pixelPosition / pixelResolution;
 		}
 	}
 }
