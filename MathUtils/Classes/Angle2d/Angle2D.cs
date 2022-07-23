39d546e13f80fbdee28b49954acd92869c5765c8
HectorColasValtueña <MephistoII666@gmail.com>
2022-07-24 00:00:42 +0200

    IAngle2d removed set properties, Angle2D made readonly struct

--- MathUtils/Classes/Angle2d/Angle2D.cs
+++ MathUtils/Classes/Angle2d/Angle2D.cs
@@ -4,67 +4,50 @@ namespace PHATASS.Utils.MathUtils
 {
 	// Represents an angle, both accessible as degrees or radians
 	// value is automatically clamped to 0-360 degrees (0 - 2pi rads)
-	public class Angle2D : IAngle2D
+	public readonly struct Angle2D : IAngle2D
 	{
 	//IAngle2D Implementation
-		float IAngle2D.degrees
-		{
-			get { return this.degrees; }
-			set { this.degrees = value; }
-		}
-
-		float IAngle2D.radians
-		{
-			get { return this.radians; }
-			set { this.radians = value; }
-		}
+		float IAngle2D.degrees { get { return this.degrees; }}
+		float IAngle2D.radians { get { return this.radians; }}
 	//ENDOF IAngle2D
 
 	//Constructor
-		//constructor takes no parameters to avoid degrees/radians ambiguity
+		//no public constructor to avoid ambiguity between degrees and radians
 		//construction must be made through one of the properly named static factory methods
-		public Angle2D () {}
+		private Angle2D (float __degrees)
+		{
+			this._degrees = Angle2D.ClampDegrees(__degrees);
+		}
 
 		public static Angle2D FromDegrees (float degrees)
 		{
-			Angle2D angle = new Angle2D();
-			angle.degrees = degrees;
-			return angle;
+			return new Angle2D(degrees);
 		}
 
 		public static Angle2D FromRadians (float radians)
 		{
-			Angle2D angle = new Angle2D();
-			angle.radians = radians;
-			return angle;
+			return new Angle2D(radians * Mathf.Rad2Deg);
 		}
 
-		public static Angle2D FromAngle (Angle2D originalAngle)
+		public static Angle2D FromAngle2D (Angle2D originalAngle)
 		{
-			Angle2D angle = new Angle2D();
-			angle.degrees = originalAngle.degrees;
-			return angle;
+			return Angle2D.FromDegrees(originalAngle.degrees);
 		}
 	//ENDOF Constructor
 
 
 	//private properties
-		private float degrees
-		{
-			get { return this._degrees; }
-			set { this._degrees = this.ClampDegrees(degrees); }
-		}
-		private float _degrees = 0.0f;
+		private readonly float _degrees;
 
-		private float radians
-		{
-			get { return this.degrees * Mathf.Deg2Rad; }
-			set { this.degrees = value * Mathf.Rad2Deg; }
-		}
+		private float degrees { get { return this._degrees; }}
+		private float radians {	get { return this.degrees * Mathf.Deg2Rad; }}
 	//ENDOF private properties
 
+	//private fields
+	//private fields
+
 	//private methods
-		private float ClampDegrees (float degrees)
+		private static float ClampDegrees (float degrees)
 		{
 			float clampedDegrees = degrees % 360;
 
--- MathUtils/Classes/Angle2d/IAngle2D.cs
+++ MathUtils/Classes/Angle2d/IAngle2D.cs
@@ -2,7 +2,7 @@ namespace PHATASS.Utils.MathUtils
 {
 	public interface IAngle2D
 	{
-		float degrees { get; set; }
-		float radians { get; set; }
+		float degrees { get; }
+		float radians { get; }
 	}
 }
