366a5830fcb32b47d6305e08de2915cdb346564f
HectorColasValtueña <MephistoII666@gmail.com>
2022-11-23 13:57:36 +0100

    Created IToggleable

---
+++ Types/Toggleable/IToggleable.cs
@@ -0,0 +1,8 @@
+namespace PHATASS.Utils.Types
+{
+	// Interface representing an element that can be toggled on/off (true/false)
+	public interface IToggleable
+	{
+		bool toggleState { get; set; }
+	}
+}
