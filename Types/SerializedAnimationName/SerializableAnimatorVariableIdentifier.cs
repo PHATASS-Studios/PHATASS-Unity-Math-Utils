442c13800a675b1bc008746d6b23e7b790272da0
HectorColasValtueña <MephistoII666@gmail.com>
2022-12-28 19:39:31 +0100

    ?? added implicit conversion operator string => SerializableAnimatorVariableIdentifier

--- Types/SerializedAnimationName/SerializableAnimatorVariableIdentifier.cs
+++ Types/SerializedAnimationName/SerializableAnimatorVariableIdentifier.cs
@@ -5,7 +5,7 @@ namespace PHATASS.Utils.Types
 	//Class handling the (de)serialization of the identifier for a UnityEngine.Animator variable name
 	//this serializes the variable name as a string for editor purposes, then exposes the identifier as the more efficient hash
 	[System.Serializable]
-	public struct SerializableAnimatorVariableIdentifier :
+	public readonly struct SerializableAnimatorVariableIdentifier :
 		IAnimatorVariableIdentifier,
 		ISerializationCallbackReceiver
 	{
@@ -21,11 +21,24 @@ namespace PHATASS.Utils.Types
 	//ISerializationCallbackReceiver
 		//OnBeforeSerialize empty - no help needed with serialization
 		void ISerializationCallbackReceiver.OnBeforeSerialize () {}
-
 		//cache variable name's hash on deserialization
-		void ISerializationCallbackReceiver.OnAfterDeserialize ()
-		{ this.variableID = Animator.StringToHash(this.variableName); }
+		void ISerializationCallbackReceiver.OnAfterDeserialize () { this.Init(this.variableName); }
 	//ENDOF ISerializationCallbackReceiver
+
+	//Operators
+		public static implicit operator SerializableAnimatorVariableIdentifier(string name)
+		{ return new SerializableAnimatorVariableIdentifier(name: name); }
+	//ENDOF Operators
+
+	//Constructor 
+		public SerializableAnimatorVariableIdentifier (string name)
+		{	
+			this.variableName = name;
+			this.Init(name);
+		}
+		private Init (string name)
+		{ this.variableID = Animator.StringToHash(name); }
+	//ENDOF Constructor
 
 	//private fields
 		private int variableID;
