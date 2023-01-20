da057aa7333bf6de199ed8c5e0ce195c6a5d150f
HectorColasValtueña <MephistoII666@gmail.com>
2023-01-20 02:05:27 +0100

    Migrated some extension methods to TransformExtensions

--- Extensions/TransformExtensions.cs
+++ Extensions/TransformExtensions.cs
@@ -1,9 +1,19 @@
 using Transform = UnityEngine.Transform;
+using Component = UnityEngine.Component;
 
 namespace PHATASS.Utils.Extensions
 {
 	public static class TransformExtensions
 	{
+	//Transform configuration extensions
+		public static void ESetTagAndLayer (this Transform transform, string targetTag, int targetLayer)
+		{
+			if (targetTag != null) { transform.gameObject.tag = targetTag; }
+			if (targetLayer >= 0) { transform.gameObject.layer = targetLayer; }
+		}
+	//ENDOF Transform configuration extensions
+
+	//Destruction methods
 		//deletes every child transform this transform is parent of
 		public static void EDestroyAllChildren (this Transform transform)
 		{
@@ -17,5 +27,36 @@ namespace PHATASS.Utils.Extensions
 			for (int i = transform.childCount-1; i >= 0; i--)
 			{ UnityEngine.Object.DestroyImmediate(transform.GetChild(i).gameObject); }
 		}
+	//ENDOF Destruction methods
+
+	//Component configuration methods
+		//Ensures transform contains one component of type T and applies sample settings if received. Creates a new TComponent if one did not exist.
+		//Returns the TComponent found or created.
+		public static TComponent ESetupComponent <TComponent> (this Transform transform, TComponent sample)
+			where TComponent: Component
+		{
+			if (sample == null) { return transform.ESetupComponent<TComponent>(); }
+			return transform.ESetupComponent<TComponent>().EApplySettingsGeneric<TComponent>(sample);
+		}
+		public static TComponent ESetupComponent <TComponent> (this Transform transform)
+			where TComponent: Component
+		{
+			TComponent component = transform.gameObject.GetComponent<TComponent>();
+			
+			if (component == null) { component = transform.ECreateComponent<TComponent>(); }
+			return component;
+		}
+
+		//Simply creates a new component of type TComponent on target transform using the most adequate implementation
+		public static TComponent ECreateComponent <TComponent> (this Transform transform)
+			where TComponent: Component
+		{
+			#if UNITY_EDITOR
+				return UnityEditor.ObjectFactory.AddComponent<TComponent>(transform.gameObject);
+			#else
+				return transform.gameObject.AddComponent<TComponent>();
+			#endif
+		}
+	//ENDOF Component configuration methods
 	}
 }
