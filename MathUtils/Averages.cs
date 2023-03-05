68d6cbee9b6ba324ccfd3cbe8bcd92d0a298fec7
HectorColasValtueña <MephistoII666@gmail.com>
2023-03-05 10:03:46 +0100

    Averages class added Vector2WeightedArithmeticAverage

--- MathUtils/Averages.cs
+++ MathUtils/Averages.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
 
+using Vector2 = UnityEngine.Vector2;
+
 namespace PHATASS.Utils.MathUtils
 {
 	public static class Averages
 	{
-		//Takes an enumeration of pairs of value/weight values and calculates their weighted arithmetic average
-		//if no entries or total weight is 0 returns 0
-		public static float WeightedArithmeticAverage(IEnumerable<(float value,float weight)> weightedValuesEnumerable)
+	//Weighted Averages
+	//	Takes an enumeration of pairs of value/weight values and calculates their weighted arithmetic average
+	//	if no entries or total weight is 0 returns 0
+		public static float FloatWeightedArithmeticAverage (IEnumerable<(float value,float weight)> weightedValuesEnumerable)
 		{
 			float totalValue = 0f;
 			float totalWeight = 0f;
@@ -20,5 +23,21 @@ namespace PHATASS.Utils.MathUtils
 			if (totalWeight == 0) { return 0; }
 			else { return totalValue/totalWeight; }
 		}
+
+		public static Vector2 Vector2WeightedArithmeticAverage (IEnumerable<(Vector2 value,float weight)> weightedValuesEnumerable)
+		{
+			Vector2 totalValue = Vector2.zero;
+			float totalWeight = 0f;
+
+			foreach((Vector2 value, float weight) weightedValuePair in weightedValuesEnumerable)
+			{
+				totalValue += weightedValuePair.value * weightedValuePair.weight;
+				totalWeight += weightedValuePair.weight;
+			}
+
+			if (totalWeight == 0) { return Vector2.zero; }
+			else { return totalValue/totalWeight; }
+		}
+	//ENDOF Weighted Averages
 	}
 }
