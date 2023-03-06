0625af633bbbf579c0e34c08eb0b8d9dda5ce22b
HectorColasValtueña <MephistoII666@gmail.com>
2023-03-06 08:54:55 +0100

    Averages added float and Vector2 arithmetic averages

--- MathUtils/Averages.cs
+++ MathUtils/Averages.cs
@@ -6,6 +6,40 @@ namespace PHATASS.Utils.MathUtils
 {
 	public static class Averages
 	{
+	//Arithmetic averages
+	//	Takes an enumeration of values and calculates their average
+	//	if no entries returns 0
+		public static float FloatArithmeticAverage (IEnumerable<float> valuesEnumerable)
+		{
+			float totalValue = 0f;
+			int entries = 0;
+
+			foreach (float value in valuesEnumerable)
+			{
+				totalValue += value;
+				entries++;
+			}
+
+			if (entries <= 0) { return 0f; }
+			else { return totalValue / entries; }
+		}
+
+		public static Vector2 Vector2ArithmeticAverage (IEnumerable<Vector2> valuesEnumerable)
+		{
+			Vector2 totalValue = Vector2.zero;
+			int entries = 0;
+
+			foreach (Vector2 value in valuesEnumerable)
+			{
+				totalValue += value;
+				entries++;
+			}
+
+			if (entries <= 0) { return Vector2.zero; }
+			else { return totalValue / entries; }
+		}
+	//ENDOF Arithmetic averages
+
 	//Weighted Averages
 	//	Takes an enumeration of pairs of value/weight values and calculates their weighted arithmetic average
 	//	if no entries or total weight is 0 returns 0
