908cca8d302372c3b95f0543bb91f28a7b31967b
HectorColasValtueña <MephistoII666@gmail.com>
2023-03-07 15:40:56 +0100

    OvoidBoundary2dBehaviour removed and replaced by two serializable classes OvoidBoundary2D & OvoidBoundary2DTransform

--- Types/Boundaries/OvoidBoundary2D.cs
+++ Types/Boundaries/OvoidBoundary2D.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
+using Vector2 = UnityEngine.Vector2;
+
 using static PHATASS.Utils.Types.Boundaries.Boundary2DEnumerators;
 
 using IAngle2D = PHATASS.Utils.Types.Angles.IAngle2D;
@@ -11,23 +13,16 @@ using static PHATASS.Utils.Types.Angles.IAngle2DFactory;
 using static PHATASS.Utils.Extensions.Vector2Extensions;
 using static PHATASS.Utils.Extensions.FloatExtensions;
 
-using Averages = PHATASS.Utils.MathUtils.Averages;
-
 namespace PHATASS.Utils.Types.Boundaries
 {
 	//This class calculates an asymetrical ellipsoid boundary
 	//each quarter has it's curvature defined by its two neigboring limits
-	//Receives and returns values in world space if useCenterTransformLocalSpace is true
-	public class OvoidBoundary2DBehaviour :
-		UnityEngine.MonoBehaviour,
+	//Receives and returns values in local space
+	[System.Serializable]
+	public class OvoidBoundary2D :
 		IBoundary2D
 	{
 	//Serialized fields
-	/*	[Tooltip("Center reference point. This transform will be considered the center of the boundary")]
-		[SerializeField]
-		private Transform centerTransform;
-		*/
-
 		[Tooltip("X Axis upper apex")]
 		[SerializeField]
 		private float xUpperRadius;
@@ -47,66 +42,59 @@ namespace PHATASS.Utils.Types.Boundaries
 
 	//IBoundary2D
 		//position of the center of the boundaries
-		Vector2 IBoundary2D.center { get { return this.center; }}
+		Vector2 IBoundary2D.center { get { return this.publicCenter; }}
+		protected virtual Vector2 publicCenter { get { return this.center; }}
 
 		//returns true if point is in or on the boundaries defined
-		bool IBoundary2D.Contains (Vector2 point) { return this.Contains(point); }
+		bool IBoundary2D.Contains (Vector2 point) { return this.PublicContains(point); }
+		protected virtual bool PublicContains (Vector2 point) { return this.Contains(point); }
 
 		//returns the closest point to target that is in or on bounds
-		Vector2 IBoundary2D.Clamp (Vector2 point) { return this.Clamp(point);  }
+		Vector2 IBoundary2D.Clamp (Vector2 point) { return this.PublicClamp(point); }
+		protected virtual Vector2 PublicClamp (Vector2 point) { return this.Clamp(point); }
 
 		//returns a point between the center (distance 0) and boundaries (distance 1)
 		//point is projected in angle direction from bounds center
-		Vector2 IBoundary2D.PointAtAngleFromCenter (float normalizedDistance, IAngle2D degrees)
-		{ return this.PointAtAngleFromCenterWorldSpace(normalizedDistance, degrees); }
+		Vector2 IBoundary2D.PointAtAngleFromCenter (float normalizedDistance, IAngle2D angle) { return this.PublicPointAtAngleFromCenter(normalizedDistance, angle); }
+		protected virtual Vector2 PublicPointAtAngleFromCenter (float normalizedDistance, IAngle2D angle)
+		{ return this.PointAtAngleFromCenter(normalizedDistance, angle); }
 
 		//transforms a point into a value representing its distance from the center
 		//returns 0 for the center point, 1 for any value exactly on the bounds, and >1 for items outside bounds, in proportion to its distance to the center
-		float IBoundary2D.PointToNormalizedDistanceFromCenter (Vector2 point)
-		{ return this.PointToNormalizedDistanceFromCenterWorldSpace(point); }
+		float IBoundary2D.PointToNormalizedDistanceFromCenter (Vector2 point) { return this.PublicPointToNormalizedDistanceFromCenter(point); }
+		protected virtual float PublicPointToNormalizedDistanceFromCenter (Vector2 point)
+		{ return this.PointToNormalizedDistanceFromCenter(point); }
 
 		//returns the distance from center to the boundaries in target direction
-		float IBoundary2D.RadiusAtAngleFromCenter (IAngle2D angle)
-		{ return this.RadiusAtAngleFromCenterLocalSpace(angle + this.boundaryRotation); }
+		float IBoundary2D.RadiusAtAngleFromCenter (IAngle2D angle) { return this.PublicRadiusAtAngleFromCenter(angle); }
+		protected virtual float PublicRadiusAtAngleFromCenter (IAngle2D angle)
+		{ return this.RadiusAtAngleFromCenter(angle); }
 
 		//Iterates over points of the boundary. gives exactly totalPoints points, which are meant to be equidistant around the shape
 		IEnumerable<Vector2> IBoundary2D.EnumerateBoundaryPoints (ushort totalPoints)
 		{ return new Boundary2DPerimeterEnumerable(this, totalPoints); }
 	//ENDOF IBoundary2D
 
-	//MonoBehaviour
-		/*
-		private void Awake ()
-		{
-			//this.ValidateLimits();
-		}
-		//*/
+	//protected class members
+		private Vector2 center { get { return Vector2.zero; }}
+	//ENDOF protected
 
-	//ENDOF MonoBehaviour
-
-	//private properties
-		private Transform centerTransform { get { return this.transform; }}
-		private Vector2 center { get { return this.centerTransform.position; }}
-
-		private IAngle2D boundaryRotation { get { return this.transform.rotation.eulerAngles.z.EDegreesToAngle2D(); }}
-	//ENDOF private properties
-
-	//private methods
+	//private members
 		//transforms a point into a value representing its distance from the center
 		//returns 0 for the center point, 1 for any value exactly on the bounds, and >1 for items outside bounds, in proportion to its distance to the center
-		private float PointToNormalizedDistanceFromCenterWorldSpace (Vector2 point)
+		private float PointToNormalizedDistanceFromCenter (Vector2 point)
 		{
-			return this.DistanceToCenter(point) / this.RadiusAtAngleFromCenterLocalSpace(this.AngleFromCenterToPointWorldSpace(point));
+			return this.DistanceToCenter(point) / this.RadiusAtAngleFromCenter(this.AngleFromCenterToPoint(point));
 		}
 
 		//calculates the position of the boundary at given angle from the center
 		private Vector2 BoundsPositionAtAngleFromCenter (IAngle2D angle)
 		{
-			return angle.EAngle2DToVector2() * this.RadiusAtAngleFromCenterLocalSpace(angle);
+			return angle.EAngle2DToVector2() * this.RadiusAtAngleFromCenter(angle);
 		}
 
 		//returns the distance from center to the boundaries in target direction
-		private float RadiusAtAngleFromCenterLocalSpace (IAngle2D angle)
+		private float RadiusAtAngleFromCenter(IAngle2D angle)
 		{
 			float cosine = angle.ECosine();
 			float sine = angle.ESine();
@@ -126,19 +114,14 @@ namespace PHATASS.Utils.Types.Boundaries
 				);
 		}
 
-		//returns a point between the center (distance 0) and boundaries (distance 1). point is projected in angle direction from bounds center
-		//world space version - converts to local space and calls local space version
-		private Vector2 PointAtAngleFromCenterWorldSpace (float normalizedDistance, IAngle2D angle)
-		{ return this.centerTransform.TransformPoint(this.PointAtAngleFromCenterLocalSpace(normalizedDistance, angle)); }
-
 		//returns a point between the center (distance 0) and boundaries (distance 1)
 		//point is projected in angle direction from bounds center
-		private Vector2 PointAtAngleFromCenterLocalSpace (float normalizedDistance, IAngle2D angle)
+		private Vector2 PointAtAngleFromCenter (float normalizedDistance, IAngle2D angle)
 		{ return this.BoundsPositionAtAngleFromCenter(angle) * normalizedDistance; }
 
 		//returns true if point is in or on the boundaries defined
 		private bool Contains (Vector2 point)
-		{ return this.PointToNormalizedDistanceFromCenterWorldSpace(point) <= 1f; }
+		{ return this.PointToNormalizedDistanceFromCenter(point) <= 1f; }
 
 		//returns the closest point to target that is in or on bounds
 		private Vector2 Clamp (Vector2 point)
@@ -147,16 +130,13 @@ namespace PHATASS.Utils.Types.Boundaries
 			if (this.Contains(point)) //if the point is contained return it as is
 			{ return point; }
 			else //otherwise clamp the position to a point over the boundary
-			{ return this.PointAtAngleFromCenterWorldSpace(1f, this.AngleFromCenterToPointWorldSpace(point)); } //this.BoundsPositionAtAngleFromCenter(this.AngleFromCenterToPoint(point)); }
+			{ return this.PointAtAngleFromCenter(1f, this.AngleFromCenterToPoint(point)); } //this.BoundsPositionAtAngleFromCenter(this.AngleFromCenterToPoint(point)); }
 		}
 
 		//returns at what angle from the center given point is
-		private IAngle2D AngleFromCenterToPointLocalSpace (Vector2 point)
+		private IAngle2D AngleFromCenterToPoint (Vector2 point)
 		{ return this.center.EFromToAngle2D(point); }
 		
-		private IAngle2D AngleFromCenterToPointWorldSpace (Vector2 point)
-		{ return this.AngleFromCenterToPointLocalSpace(point) - this.boundaryRotation; }
-
 		//returns distance to center
 		private float DistanceToCenter (Vector2 point)
 		{ return (point - this.center).magnitude; }
---
+++ Types/Boundaries/OvoidBoundary2DTransform.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Vector2 = UnityEngine.Vector2;
+
+using IAngle2D = PHATASS.Utils.Types.Angles.IAngle2D;
+//using static PHATASS.Utils.Types.Angles.IAngle2DExtensions;
+using static PHATASS.Utils.Types.Angles.IAngle2DFactory;
+
+using static PHATASS.Utils.Extensions.Vector2Extensions;
+using static PHATASS.Utils.Extensions.FloatExtensions;
+
+namespace PHATASS.Utils.Types.Boundaries
+{
+	//This class calculates an asymetrical ellipsoid boundary
+	//each quarter has it's curvature defined by its two neigboring limits
+	//Receives and returns values in local space
+	[System.Serializable]
+	public class OvoidBoundary2DTransform :
+		OvoidBoundary2D
+	{
+	//Serialized fields
+		[Tooltip("Transform used as a pivot for the boundary.")]
+		[SerializeField]
+		private Transform pivotTransform;
+	//ENDOF Serialized
+
+	//Overrides
+		protected override Vector2 publicCenter { get { return base.publicCenter + (Vector2) this.transform.position; }}
+
+		protected override bool PublicContains (Vector2 point)
+		{ return base.PublicContains(this.WorldToLocal(point)); }
+
+		protected override Vector2 PublicClamp (Vector2 point)
+		{ return this.LocalToWorld(base.PublicClamp(this.WorldToLocal(point))); }
+
+		protected override Vector2 PublicPointAtAngleFromCenter (float normalizedDistance, IAngle2D angle)
+		{ return this.LocalToWorld(base.PublicPointAtAngleFromCenter(normalizedDistance, this.WorldToLocal(angle))); }
+
+		protected override float PublicPointToNormalizedDistanceFromCenter (Vector2 point)
+		{ return base.PublicPointToNormalizedDistanceFromCenter(this.WorldToLocal(point)); }
+
+		protected override float PublicRadiusAtAngleFromCenter (IAngle2D angle)
+		{ return base.PublicRadiusAtAngleFromCenter(this.WorldToLocal(angle)); }
+	//ENDOF Overrides
+
+	//private properties
+		private Transform transform { get { return this.pivotTransform; }}
+
+		private IAngle2D boundaryRotation { get { return this.transform.rotation.eulerAngles.z.EDegreesToAngle2D(); }}
+	//ENDOF private properties
+
+	//private methods
+		private Vector2 WorldToLocal (Vector2 worldVector)
+		{ return this.transform.InverseTransformPoint(worldVector); }
+
+		private Vector2 LocalToWorld (Vector2 localVector)
+		{ return this.transform.TransformPoint(localVector); }
+
+		private IAngle2D WorldToLocal (IAngle2D worldAngle)
+		{ return worldAngle - this.boundaryRotation; }
+	//ENDOF private methods
+	}
+}
