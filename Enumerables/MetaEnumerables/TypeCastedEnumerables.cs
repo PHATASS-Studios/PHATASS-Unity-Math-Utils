ce266b82caf2538b63c2f4146d1c53a75eefacb5
HectorColasValtueña <MephistoII666@gmail.com>
2023-03-18 08:55:05 +0100

    TypeCastedEnumerables fixed struct/nullability compatibility, added skipMiscasts = true optional parameter

--- Enumerables/MetaEnumerables/TypeCastedEnumerables.cs
+++ Enumerables/MetaEnumerables/TypeCastedEnumerables.cs
@@ -8,15 +8,16 @@ namespace PHATASS.Utils.Enumerables
 {
 // Enumerators allowing casted element access for any IEnumerables/IEnumerators
 //	Casts returned items to type <TOut>	
-//	miscasts return as null
+//	if skipMiscasts == false, miscasts are returned as default(TOut)/null.
+//	otherwise, elements without valid implicit cast
 	public static class TypeCastedEnumerables
 	{
 //extension methods
-		public static IEnumerator<TOut> ETypeCast <TOut> (this IEnumerator enumerator)
-		{ return new TypeCastedEnumerator<TOut>(enumerator); }
+		public static IEnumerator<TOut> ETypeCast <TOut> (this IEnumerator enumerator, bool skipMiscasts = true)
+		{ return new TypeCastedEnumerator<TOut>(enumerator, skipMiscasts); }
 
-		public static IEnumerable<TOut> ETypeCast <TOut> (this IEnumerable enumerable)
-		{ return new TypeCastedEnumerable<TOut>(enumerable); }
+		public static IEnumerable<TOut> ETypeCast <TOut> (this IEnumerable enumerable, bool skipMiscasts = true)
+		{ return new TypeCastedEnumerable<TOut>(enumerable, skipMiscasts); }
 //ENDOF extension methods	
 
 // IEnumerables/IEnumerators
@@ -26,12 +27,12 @@ namespace PHATASS.Utils.Enumerables
 		{
 		//IEnumerator
 			object IEnumerator.Current { get { return this.enumerator.Current; }}
-			bool IEnumerator.MoveNext () { return this.enumerator.MoveNext(); }
+			bool IEnumerator.MoveNext () { return this.MoveNext(); }
 			void IEnumerator.Reset () { this.enumerator.Reset(); }
 		//ENDOF IEnumerator
 
 		//IEnumerator<TOut>
-			TOut IEnumerator<TOut>.Current { get { return (this.enumerator.Current as TOut); }}
+			TOut IEnumerator<TOut>.Current { get { return this.castedCurrent; }}
 		//ENDOF IEnumerator<TOut>
 
 		//IDisposable
@@ -39,29 +40,56 @@ namespace PHATASS.Utils.Enumerables
 		//ENDOF IDisposable
 
 		//Constructor
-			public TypeCastedEnumerator (IEnumerator enumerator)
-			{ this.enumerator = enumerator; }
+			public TypeCastedEnumerator (IEnumerator enumerator, bool skipMiscasts)
+			{
+				this.enumerator = enumerator;
+				this.skipMiscasts = skipMiscasts;
+			}
 		//ENDOF Constructor
 
-		//private fields
+		//private
 			private IEnumerator enumerator;
-		//ENDOF private fields
+			private bool skipMiscasts;
+
+			private TOut castedCurrent
+			{
+				get
+				{
+					if (this.enumerator.Current is TOut)
+					{ return (TOut) this.enumerator.Current; }
+					else { return default(TOut); };
+				}
+			}
+			
+			private bool MoveNext ()
+			{
+				while (true)
+				{
+					if (this.enumerator.MoveNext() == false)
+					{ return false; }
+					if (this.skipMiscasts || this.enumerator.Current is TOut)
+					{ return true; }
+				}
+			}
+		//ENDOF private
 		}
 
 		private struct TypeCastedEnumerable<TOut> : IEnumerable<TOut>
 		{
 			IEnumerator IEnumerable.GetEnumerator ()
-			{ return new TypeCastedEnumerator<TOut>(enumerable.GetEnumerator()); }
+			{ return new TypeCastedEnumerator<TOut>(enumerable.GetEnumerator(), skipMiscasts); }
 
 			IEnumerator<TOut> IEnumerable<TOut>.GetEnumerator ()
-			{ return new TypeCastedEnumerator<TOut>(enumerable.GetEnumerator()); }
+			{ return new TypeCastedEnumerator<TOut>(enumerable.GetEnumerator(), skipMiscasts); }
 
-			public TypeCastedEnumerable (IEnumerable enumerable)
+			public TypeCastedEnumerable (IEnumerable enumerable, bool skipMiscasts)
 			{
 				this.enumerable = enumerable;
+				this.skipMiscasts = skipMiscasts;
 			}
 
 			private IEnumerable enumerable;
+			private bool skipMiscasts;
 		}
 	}
 //ENDOF IEnumerables/IEnumerators
