8915748e622a3477ffb6327666f833b62d0a3c8a
HectorColasValtueña <MephistoII666@gmail.com>
2023-03-20 02:23:35 +0100

    TransformToGameObjectEnumerables enumerables added

---
+++ Enumerables/TransformEnumerables/TransformToGameObjectEnumerables.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using IEnumerator = System.Collections.IEnumerator;
+using IEnumerable = System.Collections.IEnumerable;
+using IDisposable = System.IDisposable;
+
+using Transform = UnityEngine.Transform;
+using GameObject = UnityEngine.GameObject;
+
+namespace PHATASS.Utils.Enumerables
+{
+	public static partial class TransformEnumerables
+	{
+// TransformToGameObjectEnumerables
+//	Encapsulates an IEnumerator<Transform> as a IEnumerator<GameObject> returning each input transform's gameObject
+//
+//extension methods
+		public static IEnumerable<GameObject> EToGameObjects (this IEnumerable<Transform> enumerable)
+		{ return new TransformToGameObjectEnumerable(enumerable)}
+//ENDOF extension methods	
+
+// IEnumerables/IEnumerators
+		private struct TransformToGameObjectEnumerator : IEnumerator<GameObject>
+		{
+		//IEnumerator
+			object IEnumerator.Current { get { return this.enumerator.Current; }}
+			bool IEnumerator.MoveNext () { return this.enumerator.MoveNext(); }
+			void IEnumerator.Reset () { this.enumerator.Reset(); }
+		//ENDOF IEnumerator
+
+		//IEnumerator<TOut>
+			TOut IEnumerator<TOut>.Current { get { return this.currentGameObject; }}
+		//ENDOF IEnumerator<TOut>
+
+		//IDisposable
+			void IDisposable.Dispose () { (this.enumerator as IDisposable)?.Dispose(); }
+		//ENDOF IDisposable
+
+		//Constructor
+			public TransformToGameObjectEnumerator (IEnumerator<Transform> enumerator)
+			{ this.enumerator = enumerator; }
+		//ENDOF Constructor
+
+		//private
+			private IEnumerator<Transform> enumerator;
+			private bool skipMiscasts;
+
+			private GameObject currentGameObject { get { return this.enumerator.current?.gameObject; }}
+		//ENDOF private
+		}
+
+		private struct TransformToGameObjectEnumerable : IEnumerable<GameObject>
+		{
+			IEnumerator IEnumerable.GetEnumerator ()
+			{ return new TransformToGameObjectEnumerator(enumerable.GetEnumerator()); }
+
+			IEnumerator<GameObject> IEnumerable<GameObject>.GetEnumerator ()
+			{ return new TransformToGameObjectEnumerator(enumerable.GetEnumerator()); }
+
+			public TransformToGameObjectEnumerable (IEnumerable<Transform> enumerable)
+			{ this.enumerable = enumerable; }
+
+			private IEnumerable<Transform> enumerable;
+		}
+	}
+//ENDOF IEnumerables/IEnumerators
+}
