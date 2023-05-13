0907b5deb3bff3e481bb8a722fd16cde44bdaf4f
HectorColasValtueña <MephistoII666@gmail.com>
2023-05-13 18:03:39 +0200

    IAccumulator now implements IValue and IResettable

--- Types/Accumulators/IAccumulator.cs
+++ Types/Accumulators/IAccumulator.cs
@@ -1,6 +1,8 @@
 namespace PHATASS.Utils.Types
 {
-	public interface IAccumulator <TValueType>
+	public interface IAccumulator <TValueType> :
+		IValue<TValueType>,
+		IResettable
 	{
 		void Add (TValueType addendum);
 	}
