1ffa6f62da7917fe0a94915165bfcb234de19399
HectorColasValtueña <MephistoII666@gmail.com>
2023-05-13 18:03:55 +0200

    IntAccumulator implementation provided

---
+++ Types/Accumulators/Implementations/IntAccumulator.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace PHATASS.Utils.Types
+{
+	[System.Serializable]
+	public class IntAccumulator : IIntAccumulator
+	{
+	//Serialized fields
+		[Tooltip("Current accumulated value.")]
+		[SerializeField]
+		private int value = 0;
+	//ENDOF Serialized fields
+
+	//IAccumulator<int>
+		void PHATASS.Utils.Types.IAccumulator<int>.Add (int addendum)
+		{ this.Add(addendum); }
+	//ENDOF IAccumulator<int>
+
+	//IValue<int>
+		int PHATASS.Utils.Types.IValue<int>.value { get { return this.value; }}
+	//ENDOF IValue<int>
+
+	//IResettable
+		void PHATASS.Utils.Types.IResettable.Reset ()
+		{ this.Reset(); }
+	//ENDOF IResettable
+
+	//private
+		private void Add (int addendum)
+		{ this.value += addendum; }
+
+		private void Reset ()
+		{ this.value = 0; }
+	//ENDOF private
+	}
+}
