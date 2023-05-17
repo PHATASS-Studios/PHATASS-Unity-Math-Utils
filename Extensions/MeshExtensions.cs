f16c1944caebb5715cebc1bb17b89a589ca00651
HectorColasValtueña <MephistoII666@gmail.com>
2023-05-17 19:19:29 +0200

    MeshExtensions update comment

--- Extensions/MeshExtensions.cs
+++ Extensions/MeshExtensions.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+//using Angle2D = PHATASS.Utils.Types.Angles.Angle2D;
+//using IAngle2D = PHATASS.Utils.Types.Angles.IAngle2D;
+
 using Averages = PHATASS.Utils.MathUtils.Averages;
 
 namespace PHATASS.Utils.Extensions
@@ -7,7 +10,7 @@ namespace PHATASS.Utils.Extensions
 	//extensions for the UnityEngine.Mesh class
 	public static class MeshExtensions
 	{
-		//gets the centroid of a mesh (the average of its vertices)
+	// Gets the centroid of a mesh (the average of its vertices)
 		public static Vector3 EGetCentroid (this Mesh mesh)
 		{ return Averages.Vector3ArithmeticAverage(mesh.vertices); }
 	}
