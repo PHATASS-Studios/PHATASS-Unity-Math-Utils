185f2068eb149b8c502332ffb8a966ab0cf96f3d
HectorColasValtueña <MephistoII666@gmail.com>
2023-05-17 02:05:20 +0200

    EventReceiverEnumerableExtensions with ETriggerAll method

---
+++ Extensions/IEventReceiverEnumerableExtensions.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using PHATASS.Utils.Events;
+
+namespace PHATASS.Utils.Extensions
+{
+	public static class IEventReceiverEnumerableExtensions
+	{
+		public static void ETriggerAll <TParam0> (this IEnumerable<ISimpleEventReceiver<TParam0>> enumerable, TParam0 param0)
+		{
+			foreach (ISimpleEventReceiver<TParam0> receiver in enumerable)
+			{ receiver.Event(param0); }
+		}
+	}
+}
