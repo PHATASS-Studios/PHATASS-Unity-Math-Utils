bf32bf69fce3ae8e96e9153df3ff65d86d640ba0
HectorColasValtueña <MephistoII666@gmail.com>
2023-06-09 05:09:12 +0200

    RectExtensions added EDownscaleIfLarger

--- Extensions/RectExtensions/RectExtensions-ManipulationAndOperations.cs
+++ Extensions/RectExtensions/RectExtensions-ManipulationAndOperations.cs
@@ -58,6 +58,16 @@ namespace PHATASS.Utils.Extensions
 			Vector2 boundsToSelfScale = outerBound.size / self.size;	//calculate the bounds-by-size ratio to get necessary scale
 			return self.EScale(boundsToSelfScale.EMinimumDimension());	//scale initial rect by the largest dimension of the bounds-by-size ratio to fit outside desired rect
 		}
+
+		//ensures a rect is not larger than an outerBound rect. If it is, scale rect down until it fits inside. Doesn't change position, only size.
+		public static Rect EDownscaleIfLarger (this Rect self, Rect outerBound)
+		{
+			//if self is already smaller than outerBound, return it as is
+			if (self.size.x <= outerBound.size.x && self.size.y <= outerBound.size.y)
+			{ return self; }
+			//otherwise, scale to fit inside
+			return self.EScaleToFitInside(outerBound);
+		}
 	//ENDOF Rect scaling
 
 	//Rect point calculation
