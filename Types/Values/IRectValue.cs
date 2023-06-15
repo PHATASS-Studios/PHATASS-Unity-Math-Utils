3b2c8aa3eaacffc9d60362723e9c93725aa58c8b
HectorColasValtueña <MephistoII666@gmail.com>
2023-06-15 23:18:10 +0200

    created IRectValue

---
+++ Types/Values/IRectValue.cs
@@ -0,0 +1,6 @@
+namespace PHATASS.Utils.Types.Values
+{
+	public interface IRectValue: IValue <UnityEngine.Rect> {}
+
+	public interface IRectValueMutable : IRectValue, IValueMutable <UnityEngine.Rect> { }
+}
