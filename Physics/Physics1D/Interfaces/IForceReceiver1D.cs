0fa5e95d90070d3070c4588744799d8048df48ce
HectorColasValtueña <MephistoII666@gmail.com>
2023-06-21 18:49:29 +0200

    IForceReceiver1D now inherits IForceReceiverNDimensional<double>

--- Physics/Physics1D/Interfaces/IForceReceiver1D.cs
+++ Physics/Physics1D/Interfaces/IForceReceiver1D.cs
@@ -1,8 +1,6 @@
 namespace PHATASS.Utils.Physics.Physics1D
 {
-	public interface IForceReceiver1D
-	{
-		//Adds a 1D momentum to the body, with a force & direction depending on momentum value and sign.
-		void AddMomentum (double momentum);
-	}
+	public interface IForceReceiver1D :
+		PHATASS.Utils.Physics.IForceReceiverNDimensional <double>
+	{}
 }
