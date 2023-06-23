c523962a18187208c749cfc457a6627ab0960c94
HectorColasValtueña <MephistoII666@gmail.com>
2023-06-23 03:53:39 +0200

    remove old file

--- Physics/Physics1D/Interfaces/IPhysicsComponent1D.cs
+++
@@ -1,8 +0,0 @@
-namespace PHATASS.Utils.Physics.Physics1D
-{
-	public interface IPhysics1DComponent :
-		PHATASS.Utils.Physics.IPhysicsComponent
-	{
-		IPhysicsBody1D primarySubject { get; set; }
-	}
-}
