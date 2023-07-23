f1376288c707f5f4ea5af8934bf005d19522aa82
HectorColasValtueña <MephistoII666@gmail.com>
2023-07-23 16:53:42 +0200

    IToggleable added comment

--- Types/Toggleable/IToggleable.cs
+++ Types/Toggleable/IToggleable.cs
@@ -3,6 +3,7 @@ namespace PHATASS.Utils.Types
 	// Interface representing an element that can be toggled on/off (true/false)
 	public interface IToggleable
 	{
+		// Toggles this element on or off, or returns the currently DESIRED state
 		bool toggleState { get; set; }
 	}
 }
