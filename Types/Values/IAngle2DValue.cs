7f1039e3d72de59ad61f4567687bc5fb069027e3
HectorColasValtueña <MephistoII666@gmail.com>
2023-12-08 14:38:07 +0100

    created IAngle2DValue

---
+++ Types/Values/IAngle2DValue.cs
@@ -0,0 +1,6 @@
+namespace PHATASS.Utils.Types.Values
+{
+	public interface IAngle2DValue: IValue <IAngle2D> {}
+
+	public interface IAngle2DValueMutable : IAngle2DValue, IValueMutable <IAngle2D> { }
+}
