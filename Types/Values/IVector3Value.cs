05691fa630b70bc485d1993e9eccaa380c1b679a
HectorColasValtueña <MephistoII666@gmail.com>
2024-02-04 21:44:15 +0100

    IVector3Value fix: IVector3ValueMutable was extending IFloatValue instead of IVector3Value

--- Types/Values/IVector3Value.cs
+++ Types/Values/IVector3Value.cs
@@ -4,5 +4,5 @@ namespace PHATASS.Utils.Types.Values
 {
 	public interface IVector3Value: IValue <Vector3> {}
 
-	public interface IVector3ValueMutable : IFloatValue, IValueMutable <Vector3> { }
+	public interface IVector3ValueMutable : IVector3Value, IValueMutable <Vector3> { }
 }
